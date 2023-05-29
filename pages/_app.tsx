b9dabb7f1c738fed9c3ff959237c68f8f53083f9
Cristiano <cristianomartins96@hotmail.com>
2023-05-29 07:09:00 +0100

    Notes about Setup

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }: AppProps) {
-  const a = 1;
-
-  return <Component {...pageProps} />;
-}
