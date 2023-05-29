510213fb4a234e01a9ef7b850e942898053b69d0
Cristiano <cristianomartins96@hotmail.com>
2023-05-29 20:13:04 +0100

    Loading file -> Atention getData do server side fetch and render

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,22 @@
-function Dashboard() {
-  return <div>dashboard</div>;
+import Image from "next/image";
+
+async function getData() {
+  const res = await fetch("https://rickandmortyapi.com/api/character/2");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function Dashboard() {
+  const data = await getData();
+  return (
+    <div>
+      <Image width="100" height={100} src={data.image} alt="Morty"></Image>
+    </div>
+  );
 }
 
 export default Dashboard;
