0d1792daad138aa29f96a79d0a03bb9a5dec5c94
Cristiano <cristianomartins96@hotmail.com>
2023-05-30 11:03:28 +0100

    Page Characters - models - service - fetch

---
+++ app/characters/page.tsx
@@ -0,0 +1,13 @@
+import { getCharacters } from "./services";
+
+async function fetchCharacters() {
+  return await getCharacters();
+}
+
+async function Characters() {
+  const characters = await fetchCharacters();
+
+  return <div>{JSON.stringify(characters)}</div>;
+}
+
+export default Characters;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>Wellcome to Rick and Morty app</h1>
+      <h2>What do you want to see ?</h2>
+    </div>
+  );
 }
 
 export default App;
