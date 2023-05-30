108115cce34eaae0664e77ec48b8649df6522506
Cristiano <cristianomartins96@hotmail.com>
2023-05-30 10:03:25 +0100

    Start a new proj

--- app/dashboard/subpage/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface notPoundProps {}
-
-const NotFound: FC<notPoundProps> = ({}) => {
-  return <div>Couldn´t not find requested resource</div>;
-};
-
-export default NotFound;
