d644473e53a535b2aa27230f07a8dd05cd55323b
Cristiano <cristianomartins96@hotmail.com>
2023-06-02 08:08:02 +0100

    Enum and Const in navigation

---
+++ app/models/routes.model.ts
@@ -0,0 +1,42 @@
+/* 
+Why ENUMERATORS (const)
+
+The option between `const` and `enum` is relies in limitate the future.
+Let´s take a look.
+As we can see below, `CHARACTERS` are a lot in the database of Rick and Morty, but let´s imagine that are only 2 personages the `Rick` and the `Morty`. That said we can create a tipe that only accepts one of this two personages; that way we are controling the future.
+
+-----------------------------------------------
+1) Let´s define an enum with the personages.
+export enum Personages {
+  CHARACTERS1 = "Rick",
+  CHARACTERS2 = "Morty",
+}
+
+2) Let´s attribute a type that embraces the previous enum´s.
+interface Characters {
+  Personage: Personages;
+}
+
+3) Let´s associate the personage with some category.
+const badGuy: Characters = {
+  Personage: Personages.CHARACTERS1,
+};
+
+const goodGuy: Characters = {
+  Personage: Personages.CHARACTERS2,
+};
+
+4) Let´s access the personages
+console.log(badGuy.Personage);
+
+
+-----------------------------------------------
+This way we eforce that the only two personages available are the ones present in `Personages`.  
+
+*/
+
+export const Routes = {
+  HOME: "/",
+  CHARACTERS: "information/characters",
+  LOCATION: "information/location",
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import { Routes } from "./models";
+
 function App() {
   return (
     <div>
       <h1>Wellcome to Rick and Morty app</h1>
       <h2>What do you want to see ?</h2>
+      <Link href={Routes.CHARACTERS}>Characters</Link>
     </div>
   );
 }
