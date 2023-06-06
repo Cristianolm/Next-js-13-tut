668302ffdf45b58d03b595db87628fc2c7901dcb
Cristiano <cristianomartins96@hotmail.com>
2023-06-06 13:57:51 +0100

    Setup Tailwind

--- app/components/Navigator/Navigator.tsx
+++ app/components/Navigator/Navigator.tsx
@@ -9,7 +9,11 @@ function Navigator({ pathNames }: Props) {
   return (
     <div style={{ display: "flex", gap: "15px", flexDirection: "row" }}>
       {pathNames.map((pathName, index) => (
-        <Link key={index} href={pathName.path}>
+        <Link
+          className="bg-fuchsia-500 hover:bg-sky-700"
+          key={index}
+          href={pathName.path}
+        >
           {pathName.name}
         </Link>
       ))}
