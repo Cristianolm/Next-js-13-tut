53cd259caa81098b51ceb35deb090b4e776fdd11
Cristiano <cristianomartins96@hotmail.com>
2023-06-09 16:14:03 +0100

    Google Fonts in NextJs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 // These styles apply to every route in the application
 import "./tailwind-globals.scss";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["italic", "normal"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  display: "optional",
+});
+
+/* 
+display:"swap" -> While the Google font doesn't finish downloading, the fonts in `globals.css` should be considered. But when the download is finished, it switches to the `Roboto` font.
+
+display:"optional" -> Until the font download from Google Fonts is finished, no text should be displayed.
+*/
 
 export const metadata = {
   title: "Learn Next JS",
@@ -12,7 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html>
+      <head lang="en" className={roboto.className} />
       <body>{children}</body>
     </html>
   );
