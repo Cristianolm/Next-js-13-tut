d9cd3288152f13cc70411246961f1469db5aab41
Cristiano <cristianomartins96@hotmail.com>
2023-06-09 16:30:46 +0100

    Internal Fonts in NextJs

--- app/information/layout.tsx
+++ app/information/layout.tsx
@@ -1,7 +1,29 @@
 import cardLayoutStyles from "./information-layout.module.scss";
+import localFont from "@next/font/local";
+
+const firaCode = localFont({
+  src: [
+    {
+      path: "./FiraCode-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./FiraCode-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 function CardLayout({ children }: { children: React.ReactNode }) {
-  return <div className={cardLayoutStyles.informationLayout}>{children}</div>;
+  return (
+    <div
+      className={`${firaCode.className} ${cardLayoutStyles.informationLayout}`}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default CardLayout;
